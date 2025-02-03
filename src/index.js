0e360f46099f992101ab91e13f4112bdeac0f754
yuri-italo <yuri.italo94@gmail.com>
2025-02-03 14:40:44 -0300

    feat: add project entity

--- src/index.js
+++ src/index.js
@@ -1,4 +1,34 @@
 import Todo from "./todo.js";
+import Project from "./project.js";
 
-const todo = new Todo("Do the dishes", "I need to do it now", "2025-08-07", "High", "Nothing to say");
-console.log(todo);
+const todo = new Todo(
+  "Do the dishes",
+  "I need to do it now",
+  "2025-08-07",
+  "High",
+  "Nothing to say"
+);
+
+const anotherTodo = new Todo(
+  "Buy groceries",
+  "Get milk, eggs, and bread",
+  "2025-08-08",
+  "Medium",
+  "Check for discounts"
+);
+
+// add
+const project = new Project();
+project.add(todo);
+console.log(project.todoList);
+project.add(anotherTodo);
+console.log(project.todoList);
+
+// remove
+const removed = project.remove(1);
+console.log(project.todoList);
+console.log(removed);
+
+// edit
+project.edit(anotherTodo, 0);
+console.log(project.todoList);
---
+++ src/project.js
@@ -0,0 +1,59 @@
+import Todo from "./todo.js";
+
+export default class Project {
+  #name;
+  #todoList;
+
+  constructor(name = "default") {
+    this.name = name;
+    this.#todoList = [];
+  }
+
+  add(todo) {
+    if (Object.getPrototypeOf(todo) !== Todo.prototype) {
+      throw new Error("Invalid Todo");
+    }
+
+    this.#todoList.push(todo);
+  }
+
+  edit(todo, todoIndex) {
+    if (Object.getPrototypeOf(todo) !== Todo.prototype) {
+      throw new Error("Invalid Todo");
+    }
+
+    if (typeof todoIndex !== "number" || todoIndex < 0) {
+      throw new Error("Invalid todo index");
+    }
+
+    if (this.#todoList[todoIndex]) {
+      this.#todoList[todoIndex] = todo;
+    } else {
+      throw new Error(`The todo at index ${todoIndex} does not exist`);
+    }
+  }
+
+  remove(todoIndex) {
+    if (typeof todoIndex !== "number" || todoIndex < 0) {
+      throw new Error("Invalid todo index");
+    }
+
+    return this.#todoList.splice(todoIndex, 1);
+  }
+
+  set name(newName) {
+    if (typeof newName === "string" && newName.trim() !== "") {
+      this.#name = newName.trim();
+    } else {
+      throw new Error("Name must be a non-empty string.");
+    }
+  }
+
+  get name() {
+    return this.#name;
+  }
+
+  get todoList() {
+    return [...this.#todoList];
+  }
+}
